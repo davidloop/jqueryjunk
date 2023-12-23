01ba5b9ef3b2ccd298871cbf882710c43e52dc6a
David Loop <davidloop@gmail.com>
2023-12-23 14:33:41 -0600

    Refactor toggle animation code

--- js/simple-toggle-animate-to-top.js
+++ js/simple-toggle-animate-to-top.js
@@ -1,20 +1,27 @@
 (function($) {
 
-    $(".simple-toggle-to-top-trigger").on('click', (e) => {
+    $(".simple-toggle-to-top-trigger").on('click', function(e) {
         e.preventDefault();
 
-        let target = $(e.currentTarget).parent().find(".simple-toggle-to-top-target");
+        var target = $(this).parent().find(".simple-toggle-to-top-target");
 
-        if ( $(e.currentTarget).hasClass('open') ) {
-            $(e.currentTarget).removeClass('open').attr('aria-expanded', 'false');
-            $(target).slideUp('fast');
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open').attr('aria-expanded', 'false');
+            target.slideUp('fast');
         } else {
-            $(e.currentTarget).addClass('open').attr('aria-expanded', 'true');
-            $(target).slideDown('fast');
+            $(this).addClass('open').attr('aria-expanded', 'true');
+            target.slideDown('fast');
 
             $("body, html").animate({
-                scrollTop: $(e.currentTarget).offset().top - 105
+                scrollTop: $(this).offset().top - 105
             }, 200);
+        }
+    });
+
+    $(".simple-toggle-to-top-trigger").on('keydown', function(e) {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            $(this).trigger('click');
         }
     });
 
