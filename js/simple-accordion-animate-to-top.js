fe481cb305af2625f9b22bf60d9103c02c2c3965
David Loop <davidloop@gmail.com>
2023-12-23 20:10:16 -0600

    Refactor simple-accordion-to-top-trigger event handler

--- js/simple-accordion-animate-to-top.js
+++ js/simple-accordion-animate-to-top.js
@@ -1,25 +1,27 @@
 (function($) {
+    $(".simple-accordion-to-top-trigger").on('click keypress', (e) => {
+        if (e.type === 'keypress' && e.key !== 'Enter') {
+            return;
+        }
 
-    $(".simple-accordion-to-top-trigger").on('click', (e) => {
         e.preventDefault();
 
-        let target = $(e.currentTarget).parent().find(".simple-accordion-to-top-target");
+        const target = $(e.target).parent().find(".simple-accordion-to-top-target");
 
-        if ( $(e.currentTarget).hasClass('open') ) {
-            $(e.currentTarget).removeClass('open').attr('aria-expanded', 'false');
+        if ($(e.target).hasClass('open')) {
+            $(e.target).removeClass('open').attr('aria-expanded', 'false');
             $(target).slideUp('fast');
         } else {
             $(".simple-accordion-to-top-trigger").removeClass('open').attr('aria-expanded', 'false');
             $(".simple-accordion-to-top-target").slideUp('fast');
-            $(e.currentTarget).addClass('open').attr('aria-expanded', 'true');
+            $(e.target).addClass('open').attr('aria-expanded', 'true');
             $(target).slideDown('fast');
         
             setTimeout( ()=> {
                 $("body, html").animate({
-                    scrollTop: $(e.currentTarget).offset().top - 105
+                    scrollTop: $(e.target).offset().top - 105
                 }, 200);
             }, 200);
         }
     });
-
 })(jQuery);
